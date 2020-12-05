54450c2420210e1bcd7b6fe8e4f7a0d5c100d8e1
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-05 10:33:41 -0600

    Form component was added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import {Fragment} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Fragment>
+      <Header 
+        title={'Clima React'}
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form />
+            </div>
+            
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,54 @@
+import {useState} from 'react';
+
+const Form = () => {
+  const [search, setSearch] = useState({
+    city: '',
+    country: '',
+  });
+
+  const {city, country} = search;
+
+  const handleChange = e => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form
+    >
+      <div className="input-field col s12">
+        <input 
+          type="text"
+          name="city"
+          id="city"
+          value={city}
+          onChange={handleChange}
+        />
+        <label htmlFor="city">Ciudad: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <select
+          name="country"
+          id="country"
+          value={country}
+          onChange={handleChange}
+        >
+          <option value="">-- Seleccione un país --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor="country">País: </label>
+      </div>
+    </form>
+  );
+}
+ 
+export default Form;
